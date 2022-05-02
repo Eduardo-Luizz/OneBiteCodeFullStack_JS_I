ab7bad56f10de36d955a4abef2bb3f9cc6ead970
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-05-02 16:10:08 -0300

    Praticando com Arrays

---
+++ JavaScript/13_Praticando_arrays/index.js
@@ -0,0 +1,3 @@
+const hitchedSpaceShips = [["Fenix", 8, true], ["Golias", 10, true], ["Helmet", 5, false], ["Elemental", 3, true], ["Darwin", 15, false]]
+// Plataforma de engate [é o indice  + 1]
+
