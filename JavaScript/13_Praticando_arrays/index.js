26e59e2d87db831357326a71557d2acae0d61e50
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-05-02 16:29:57 -0300

    Mudando a forma de resolução

--- JavaScript/13_Praticando_arrays/index.js
+++ JavaScript/13_Praticando_arrays/index.js
@@ -1,3 +1,5 @@
 const hitchedSpaceShips = [["Fenix", 8, true], ["Golias", 10, true], ["Helmet", 5, false], ["Elemental", 3, true], ["Darwin", 15, false]]
 // Plataforma de engate [é o indice  + 1]
 
+let teste = hitchedSpaceShips.filter(element => {return element})
+console.log(teste)
