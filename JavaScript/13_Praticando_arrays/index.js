ac0bbcb8f812695d4ca8cfaa9dc2bbe2dfedf073
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-05-11 15:39:15 -0300

    Alteração

--- JavaScript/13_Praticando_arrays/index.js
+++ JavaScript/13_Praticando_arrays/index.js
@@ -1,5 +1,28 @@
-const hitchedSpaceShips = [["Fenix", 8, true], ["Golias", 10, true], ["Helmet", 5, false], ["Elemental", 3, true], ["Darwin", 15, false]]
-// Plataforma de engate [é o indice  + 1]
+const hitchedSpaceShips = 
+[
+  ["Fenix", 8, true], 
+  ["Golias", 10, true], 
+  ["Helmet", 5, false], 
+  ["Elemental", 3, true], 
+  ["Darwin", 15, false]
+]
 
-let teste = hitchedSpaceShips.filter(element => {return element})
-console.log(teste)
+let crew = hitchedSpaceShips.filter(element => {
+  return element[1] >= 9
+}).map(element => {
+  return element[0]
+})
+
+let onGoingHitchPlatform = hitchedSpaceShips.findIndex(element => {
+  return element[2] === false
+})
+
+let highLightSpace = hitchedSpaceShips.map(element => {
+  return element[0].toUpperCase()
+})
+
+let message = "Espaçonaves com mais de 9 tripulantes: " + crew.join(", ")
+message += "\nPlataforma com processo de engate: " + (onGoingHitchPlatform + 1)
+message += "\nEspaçonaves destacadas: " + highLightSpace.join(", ")
+
+alert(message)
