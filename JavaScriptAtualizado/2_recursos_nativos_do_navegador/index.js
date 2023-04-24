4e8a9d3b07c813ad37c8b1bb9441dabcd4fce8d3
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-24 10:35:28 -0300

    Estudando recursos nativos do navegador

---
+++ JavaScriptAtualizado/2_recursos_nativos_do_navegador/index.js
@@ -0,0 +1,12 @@
+let nome = "Eduardo"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+
+window.alert(nome)
+teste = window.prompt("Insira o seu nome: ")
+console.log(teste)
+teste2 = window.confirm("Confirma está ação ?")
+console.log(teste2)
