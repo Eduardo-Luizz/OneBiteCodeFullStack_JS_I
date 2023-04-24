3ccd91b9ab61983a08996766a2d9bb4268c378ea
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-24 11:08:20 -0300

    Fazendo o exercício 1

---
+++ JavaScriptAtualizado/3_exercícios/1_cadastro_de_usuário/index.js
@@ -0,0 +1,7 @@
+nome = window.prompt('Qual é seu nome: ')
+sobrenome = window.prompt('Qual é seu sobrenome: ')
+campo_de_estudo = window.prompt('Qual é seu campo de estudo: ')
+ano_de_nascimento = window.prompt('Qual é seu ano de nascimento: ')
+
+window.alert(
+`Olá Sr(a): ${nome} ${sobrenome} \nSeu campo de estudo é: ${campo_de_estudo} \nSua idade é: ${2023 - ano_de_nascimento} `)
