a09a18a4f73349f3f2d6464ec887ae832d05f9d9
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-25 15:31:32 -0300

    Aula sobre estrutura de repetição for

---
+++ JavaScriptAtualizado/8_for/aula.js
@@ -0,0 +1,10 @@
+let nome = "Eduardo Luiz"
+
+for (let index = 0; index < nome.length; index++) {
+    console.log(nome[index])
+}
+
+for (let indice = 0; indice <= 10; indice++) {
+    alert(`Indice = ${indice}`)
+}
+
