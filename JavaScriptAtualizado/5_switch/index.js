70636e90b75cf17bf495771993d38aaf2ccd2097
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-25 09:28:21 -0300

    Aulas referente a switch

---
+++ JavaScriptAtualizado/5_switch/index.js
@@ -0,0 +1,17 @@
+
+const resultado = prompt("Escolha uma alternativa:\na\nb\nc")
+
+switch (resultado) {
+    case "a":
+        alert("A")
+        break;
+    case "b":
+        alert("B")
+        break;
+    case "c":
+        alert("C")
+        break;
+    default:
+        alert("Finalizando")
+        break;
+}
